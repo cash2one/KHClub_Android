9471013b4db8bf961196fddad2f6a2cdb4815a04
yukirin <yukirin_000@163.com>
2015-10-14 17:24:33 +0800

    IM3

--- KHClub_Android/src/com/app/khclub/base/app/KHApplication.java
+++ KHClub_Android/src/com/app/khclub/base/app/KHApplication.java
@@ -3,12 +3,13 @@ package com.app.khclub.base.app;
 import io.yunba.android.manager.YunBaManager;
 import android.app.Application;
 
+import com.app.khclub.base.easeim.KHHXSDKHelper;
 import com.app.khclub.base.manager.DBManager;
 import com.app.khclub.base.manager.UserManager;
 import com.app.khclub.base.utils.FileUtil;
+import com.easemob.EMCallBack;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.umeng.analytics.MobclickAgent;
 
 /**
  *Application
@@ -20,6 +21,8 @@ public class KHApplication extends Application {
 	// application
 	public static KHApplication application;
 	public static boolean isDebug;
+	
+	public static KHHXSDKHelper hxSDKHelper = new KHHXSDKHelper();
 
 	public static KHApplication getInstance() {
         return application;
@@ -42,6 +45,52 @@ public class KHApplication extends Application {
         //初始化图片加载对象
         ImageLoader.getInstance().init(
 				ImageLoaderConfiguration.createDefault(getApplicationContext()));
+        hxSDKHelper.onInit(application);
 	}  
 
+	
+	/**
+	 * 获取当前登陆用户名
+	 *
+	 * @return
+	 */
+	public String getUserName() {
+	    return hxSDKHelper.getHXId();
+	}
+
+	/**
+	 * 获取密码
+	 *
+	 * @return
+	 */
+	public String getPassword() {
+		return hxSDKHelper.getPassword();
+	}
+
+	/**
+	 * 设置用户名
+	 *
+	 * @param user
+	 */
+	public void setUserName(String username) {
+	    hxSDKHelper.setHXId(username);
+	}
+
+	/**
+	 * 设置密码 下面的实例代码 只是demo，实际的应用中需要加password 加密后存入 preference 环信sdk
+	 * 内部的自动登录需要的密码，已经加密存储了
+	 *
+	 * @param pwd
+	 */
+	public void setPassword(String pwd) {
+	    hxSDKHelper.setPassword(pwd);
+	}
+
+	/**
+	 * 退出登录,清空数据
+	 */
+	public void logout(final boolean isGCM,final EMCallBack emCallBack) {
+		// 先调用sdk logout，在清理app中自己的数据
+	    hxSDKHelper.logout(isGCM,emCallBack);
+	}
 }
