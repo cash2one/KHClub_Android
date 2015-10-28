69401d78b16131bac664e29a3431dc27aa332a05
yukirin <yukirin_000@163.com>
2015-10-28 16:43:37 +0800

    other

---
+++ KHClub_Android/src/com/app/khclub/personal/ui/fragment/OtherPersonalInfoTwoFragment.java
@@ -0,0 +1,10 @@
+package com.app.khclub.personal.ui.fragment;
+
+
+public class OtherPersonalInfoTwoFragment extends OtherPersonalInfoFragment {
+	@Override
+	public int setLayoutId() {
+		// TODO Auto-generated method stub
+		return super.setLayoutId();
+	}
+}
